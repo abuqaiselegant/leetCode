e1f4787ee9c71d50222be1201cf427edfd9ffe59
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2024-11-06 23:54:45 +0000

    Time: 17 ms (67.85%), Space: 11.7 MB (98.14%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,9 +5,20 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        a = sorted(s)
+        """a = sorted(s)
         b = sorted(t)
         if a==b:
             return True
         else :
+            return False"""
+
+        if len(s) != len(t):
             return False
+        char_count = [0] * 26
+        for c in s:
+            char_count[ord(c) - ord('a')] += 1
+        for c in t:
+            char_count[ord(c) - ord('a')] -= 1
+            if char_count[ord(c) - ord('a')] < 0:
+                return False
+        return True
