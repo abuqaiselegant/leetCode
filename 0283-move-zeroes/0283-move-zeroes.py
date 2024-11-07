ca00a3fb54844eee178b4da3059acbcf2f7c3095
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2024-11-07 00:33:38 +0000

    Time: 0 ms (100%), Space: 13.1 MB (7.72%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,10 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        for i in range(len(nums)):
+
+        """for i in range(len(nums)):
             if nums[i]==0:
                 for j in range(i+1,len(nums)):
                     if nums[j]!=0:
                         nums[i],nums[j]= nums[j],nums[i]
                         break
+                        """
+        nonZero = 0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[nonZero], nums[i]=nums[i], nums[nonZero]
+                nonZero+=1
+        
                 
