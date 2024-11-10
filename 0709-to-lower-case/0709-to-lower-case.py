9abcea19e3282884691a78f19cee38e33ec0a1b7
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2024-11-10 13:20:04 +0000

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.py
+++ 0709-to-lower-case/0709-to-lower-case.py
@@ -1,4 +1,13 @@
 class Solution:
     def toLowerCase(self, s: str) -> str:
-        return s.lower()
+        """return s.lower()
+        """
+        a =[x for x in s]
+        for i in range(len(a)):
+            if ord(a[i])<91:
+                a[i] = chr(ord(a[i])+32)
+        a ="".join(a)
+        return a
+
+
         
