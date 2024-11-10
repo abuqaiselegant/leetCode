a9d89e5ca9caa7e1a43cb5b84e1e40d2656b5be7
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2024-11-10 13:20:24 +0000

    Time: 0 ms (100%), Space: 16.7 MB (5.55%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.py
+++ 0709-to-lower-case/0709-to-lower-case.py
@@ -4,7 +4,7 @@ class Solution:
         """
         a =[x for x in s]
         for i in range(len(a)):
-            if ord(a[i])<91:
+            if 64<ord(a[i])<91:
                 a[i] = chr(ord(a[i])+32)
         a ="".join(a)
         return a
