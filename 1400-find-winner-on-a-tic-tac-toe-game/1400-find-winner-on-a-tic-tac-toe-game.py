5d823dd6357a86d9e0aad2da697417ef43e99da7
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2024-11-14 00:05:58 +0000

    Time: 0 ms (100%), Space: 16.9 MB (17%) - LeetHub

--- 1400-find-winner-on-a-tic-tac-toe-game/1400-find-winner-on-a-tic-tac-toe-game.py
+++ 1400-find-winner-on-a-tic-tac-toe-game/1400-find-winner-on-a-tic-tac-toe-game.py
@@ -1,6 +1,6 @@
 class Solution:
     def tictactoe(self, moves: List[List[int]]) -> str:
-        n=3
+        """n=3
         rows,cols = [0]*n,[0]*n
         d1,d2 = 0,0
         player = 1
@@ -20,4 +20,39 @@ class Solution:
         if len(moves)==(n*n):
             return "Draw"
         else:
-            return "Pending"
+            return "Pending"""
+        board =[[0]*3 for i in range(3)]
+    
+        for i in range(len(moves)):
+            row = moves[i][0]
+            col = moves[i][1]
+        
+            if i%2==0:
+                board[row][col]=1
+            else:
+                board[row][col]=-1
+
+        diagonal1_sum =0
+        diagonal2_sum =0
+        for i in range(3):
+            if sum(board[i])==3: return "A"
+            if sum(board[i])==-3: return "B"
+        
+            col_sum = board[0][i]+board[1][i]+board[2][i]
+            if col_sum == 3:
+                return "A"
+            if col_sum ==-3:
+                return "B"
+            diagonal1_sum += board[i][i]
+            if diagonal1_sum ==3:
+                return "A"
+            if diagonal1_sum ==-3:
+                return "B"
+            diagonal2_sum +=board[i][2-i]
+            if diagonal2_sum==3:
+                return "A"
+            if diagonal2_sum==-3:
+                return "B"
+        if len(moves)==9:
+            return "Draw"
+        return "Pending"
