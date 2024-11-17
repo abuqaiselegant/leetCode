cdf5a9f466100e53fdf969ad6577b9ea3cf43724
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2024-11-17 23:48:38 +0000

    Time: 21 ms (53.05%), Space: 18.9 MB (93.21%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.py
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -2,7 +2,7 @@ class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
         l,r=0,0
         #size of s(cookise)
-        m =len(s)
+        m = len(s)
         #n is size of child list
         n = len(g)
         g.sort()
