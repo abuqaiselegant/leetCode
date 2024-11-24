70e175e84d6a8e87d8cf0782776d816ef9dbbb6b
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2024-11-24 21:20:08 +0000

    Time: 7 ms (39.31%), Space: 17.8 MB (48.86%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,12 +1,18 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
-        for i in range(n+1):
+        """for i in range(n+1):
             flag =0
             for j in range(n):
                 if nums[j]==i:
                     flag =1
                     break
             if flag==0:
+                return i"""
+        hash = [0]*(n+1)
+        for i in range(n):
+            hash[nums[i]]+=1
+        for i in range(n+1):
+            if hash[i]==0:
                 return i
 
