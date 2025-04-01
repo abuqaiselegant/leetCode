52a8f73645362501c93328ef450af290414f9a33
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2025-04-01 16:14:40 +0100

    Time: 0 ms (100%), Space: 13.3 MB (49.05%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,19 +1,12 @@
 class Solution:
-    def twoSum(self, nums, target) :
-        """a={}
-        for count,ele in enumerate(nums):
-            contemperory= target-ele
-            if contemperory in a:
-                return [a[contemperory],count]
-            a[ele]=count
-        return []"""
-        hash = {}
+
+    
+    def twoSum(self, nums, target):
+        a = {}
         for i in range(len(nums)):
-            com = target -nums[i]
-            if com in hash:
-                return [i,hash[com]]
+            com = target - nums[i]
+            if com in a:
+                return[i, a[com]]
             else:
-                hash[nums[i]]=i
+                a[nums[i]]=i
         return []
-    
-    
