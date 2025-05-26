4b605a27f7bd1167d42b24ba62af7a5547b26dc0
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2025-05-26 18:59:21 +0100

    Time: 12 ms (53.14%), Space: 17.8 MB (86.03%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,24 +1,12 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        """a = sorted(s)
-        b = sorted(t)
-        if a==b:
-            return True
-        else :
-            return False"""
-
-        if len(s) != len(t):
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
             return False
-        char_count = [0] * 26
-        for c in s:
-            char_count[ord(c) - ord('a')] += 1
-        for c in t:
-            char_count[ord(c) - ord('a')] -= 1
-            if char_count[ord(c) - ord('a')] < 0:
-                return False
-        return True
+        
+        countS = {}
+        countT = {}
+
+        for i in range(len(s)):
+            countS[s[i]]=1+countS.get(s[i],0)
+            countT[t[i]]=1+countT.get(t[i],0)
+        return countS==countT
