a6b360b9e671dc178c1d59693b0f9b4386e98a6c
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2025-06-20 10:34:26 +0100

    Time: 0 ms (100%), Space: 17.8 MB (78.57%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,22 +1,9 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        # Handle empty array
-        if not strs:
-            return ""
-            
-        # Get shortest string length
-        min_len = min(len(s) for s in strs)
-        
-        # Check each character position
-        for i in range(min_len):
-            # Get character to compare
-            char = strs[0][i]
-            
-            # Check this character in all strings
-            for string in strs[1:]:
-                if string[i] != char:
-                    # Return prefix up to this position
-                    return strs[0][:i]
-        
-        # If we get here, return the shortest string
-        return strs[0][:min_len]
+        res= ""
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i==len(s) or strs[0][i]!=s[i]:
+                    return res
+            res +=strs[0][i]
+        return res 
