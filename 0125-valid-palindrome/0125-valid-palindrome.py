45950c591553e671a728430eb5db0b5465efea30
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2025-06-21 11:13:03 +0100

    Time: 7 ms (81.44%), Space: 17.8 MB (95.12%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,13 +1,25 @@
-class Solution(object):
-    def isPalindrome(self,s):
-        s= s.lower()
-        cleaned = []
-        for c in s:
-            if c.isalnum():
-                cleaned.append(c)
-        cleaned = ''.join(cleaned)
-        if cleaned == cleaned[::-1]:
-            return True
-        else :
-            return False
-        
+import re 
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # s=re.sub(r'[^a-zA-Z0-9]','',s)
+        # s=s.lower()
+
+        # if s==s[::-1]:
+        #     return True
+        # else:
+        #     return False
+
+        left = 0
+        right = len(s)-1
+
+        while left<right:
+            while left<right and not s[left].isalnum():
+                left+=1
+            while left<right and not s[right].isalnum():
+                right-=1
+            if s[left].lower()==s[right].lower():
+                left+=1
+                right-=1
+            else:
+                return False
+        return True
