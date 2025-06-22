b4f4d57aa147a12fcbea0e325baa9d6fe1e981f4
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2025-06-22 14:03:12 +0100

    Time: 706 ms (29.53%), Space: 20.6 MB (85.46%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,27 +1,51 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
         nums.sort()
-        ans =[]
-        for i in range(len(nums)-2):
-            if i>0 and nums[i] == nums[i-1]:
+
+        for i, a in enumerate(nums):
+
+            if i > 0 and a == nums[i - 1]:
                 continue
+
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                threeSum = a + nums[l] + nums[r]
+                if threeSum > 0:
+                    r -= 1
+                elif threeSum < 0:
+                    l += 1
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+                        
+        return res
+
+
+
+
+        # for i in range(len(nums)-2):
+        #     if i>0 and nums[i] == nums[i-1]:
+        #         continue
             
-            sum = -nums[i]
-            left=i+1
-            right = len(nums)-1
+        #     sum = -nums[i]
+        #     left=i+1
+        #     right = len(nums)-1
 
-            while left < right:
-                if nums[left]+nums[right]== sum:
-                    ans.append([nums[i], nums[left],nums[right]])
-                    while left <right and nums[left]==nums[left+1]:
-                        left+=1
-                    while left <right and nums[right] == nums[right-1]:
-                        right-=1
-                    left+=1
-                    right-=1
+        #     while left < right:
+        #         if nums[left]+nums[right]== sum:
+        #             ans.append([nums[i], nums[left],nums[right]])
+        #             while left <right and nums[left]==nums[left+1]:
+        #                 left+=1
+        #             while left <right and nums[right] == nums[right-1]:
+        #                 right-=1
+        #             left+=1
+        #             right-=1
 
-                elif nums[left]+nums[right]>sum:
-                    right -=1
-                else :
-                    left+=1
-        return ans
+        #         elif nums[left]+nums[right]>sum:
+        #             right -=1
+        #         else :
+        #             left+=1
+        # return ans
