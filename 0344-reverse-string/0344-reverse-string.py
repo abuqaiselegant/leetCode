31139bb87914d5a61ed25036497cbe162f2cdded
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2025-06-23 11:40:22 +0100

    Time: 3 ms (43.69%), Space: 23.1 MB (41.9%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,15 +1,25 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
-        start = 0
-        end = len(s)-1
-        while(start<end):
-            '''temp = s[start]
-            s[start]=s[end]
-            s[end]= temp'''
-            s[start], s[end]=s[end],s[start]
-            start+=1
-            end-=1
+        l,r=0,len(s)-1
+        while l<r:
+            s[l],s[r] =s[r],s[l]
+            l+=1
+            r-=1
+        
+        
+        
+        
+        
+        # """
+        # Do not return anything, modify s in-place instead.
+        # """
+        # start = 0
+        # end = len(s)-1
+        # while(start<end):
+        #     '''temp = s[start]
+        #     s[start]=s[end]
+        #     s[end]= temp'''
+        #     s[start], s[end]=s[end],s[start]
+        #     start+=1
+        #     end-=1
         
