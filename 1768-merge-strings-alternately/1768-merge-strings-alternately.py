e1a2813925b355961d5c016729afe84409a9517a
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2025-06-23 12:14:47 +0100

    Time: 42 ms (28.42%), Space: 18 MB (8.17%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,26 +1,17 @@
-class Solution(object):
-    def mergeAlternately(self, word1, word2):
-        """
-        :type word1: str
-        :type word2: str
-        :rtype: str
-        """
-        merged = []
-        i = 0
-        j =0
-        while (i <len(word1) and j<len(word2)):
-            merged.append(word1[i])
-            merged.append(word2[j])
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        res=""
+        i,j=0,0
+        while i<len(word1) and j<len(word2):
+            res+=word1[i]
+            res+=word2[j]
             i+=1
             j+=1
-        
-        while(i<len(word1)):
-            merged.append(word1[i])
+        while i<len(word1):
+            res+=word1[i]
             i+=1
-        while(j<len(word2)):
-            merged.append(word2[j])
+        while j<len(word2):
+            res+=word2[j]
             j+=1
-
-        merged = "".join(merged)
-        return merged
+        return res
         
