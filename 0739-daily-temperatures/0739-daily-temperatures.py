03bf01136d1e35a85c5013ddb8193d449daa0d11
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2025-07-07 14:53:43 +0100

    Time: 133 ms (23%), Space: 34.8 MB (13.78%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -7,5 +7,5 @@ class Solution:
             while stack and t>stack[-1][1]:
                 stackInd,stackTemp = stack.pop()
                 res[stackInd] = i - stackInd
-            stack.append((i,t))
+            stack.append([i,t])
         return res
