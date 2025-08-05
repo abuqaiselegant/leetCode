f55a446fcd5540a31c0d91ca9ce907f9a9fc0264
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2025-08-05 21:33:20 +0100

    Time: 2 ms (19.52%), Space: 12.4 MB (82.08%) - LeetHub

---
+++ 0917-reverse-only-letters/0917-reverse-only-letters.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def reverseOnlyLetters(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        l , r = 0, len(s)-1
+        sList = list(s)
+        while l<=r:
+            if sList[l].isalpha() and sList[r].isalpha():
+                temp = sList[l]
+                sList[l]=sList[r]
+                sList[r] = temp
+                l+=1
+                r-=1
+            elif sList[l].isalpha()==False:
+                l+=1
+            else:
+                r-=1
+        return "".join(sList)
+        
