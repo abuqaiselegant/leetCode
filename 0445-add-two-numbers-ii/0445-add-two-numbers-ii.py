0071760c480bf37f5feb2edcb128f3a53f77c5c2
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2025-08-10 23:23:05 +0100

    Time: 15 ms (34.61%), Space: 12.6 MB (15.27%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
@@ -10,36 +10,69 @@ class Solution(object):
         :type l2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        def reverse(head):
-            pre = None
-            temp = head
-            while temp:
-                nextNode = temp.next
-                temp.next = pre
-                pre = temp
-                temp = nextNode
-            return pre
-        l1 = reverse(l1)
-        l2 = reverse(l2)
+
+        s1 = []
+        s2 = []
+
+        temp= l1
+        while temp:
+            s1.append(temp.val)
+            temp = temp.next
+        temp = l2
+        while temp:
+            s2.append(temp.val)
+            temp = temp.next
         
+        total = 0
         carry = 0
         head = None
-        p,q= l1,l2
-        while p or q or carry:
-            x= p.val if p else 0
-            y= q.val if q else 0
+        while s1 or s2 or carry:
+            x= s1.pop() if s1 else 0
+            y= s2.pop() if s2 else 0
             total = x+y+carry
-            carry = total//10
             digit = total%10
-            
-            node = ListNode(digit)
-            node.next =head
-            head = node
+            carry = total//10
 
-            if p: p =p.next
-            if q: q= q.next
-        
+            node = ListNode(digit)
+            node.next = head
+            head = node
         return head
+
+
+
+
+
+
+        # def reverse(head):
+        #     pre = None
+        #     temp = head
+        #     while temp:
+        #         nextNode = temp.next
+        #         temp.next = pre
+        #         pre = temp
+        #         temp = nextNode
+        #     return pre
+        # l1 = reverse(l1)
+        # l2 = reverse(l2)
+        
+        # carry = 0
+        # head = None
+        # p,q= l1,l2
+        # while p or q or carry:
+        #     x= p.val if p else 0
+        #     y= q.val if q else 0
+        #     total = x+y+carry
+        #     carry = total//10
+        #     digit = total%10
+            
+        #     node = ListNode(digit)
+        #     node.next =head
+        #     head = node
+
+        #     if p: p =p.next
+        #     if q: q= q.next
+        
+        # return head
 
         
 
