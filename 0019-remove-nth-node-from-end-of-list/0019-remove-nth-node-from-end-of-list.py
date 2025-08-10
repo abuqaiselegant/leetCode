031513ba3aaa13c6c06b5a1e63f3440d5214cf5b
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2025-08-10 17:04:16 +0100

    Time: 0 ms (100%), Space: 12.5 MB (56.32%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -10,16 +10,34 @@ class Solution(object):
         :type n: int
         :rtype: Optional[ListNode]
         """
-        dummy = ListNode(0,head)
+        # dummy = ListNode(0,head)
 
-        count = 0
-        temp = head
-        while temp:
-            count+=1
-            temp = temp.next
+        # count = 0
+        # temp = head
+        # while temp:
+        #     count+=1
+        #     temp = temp.next
 
-        pre = dummy
-        for i in range(count-n):
-            pre = pre.next
-        pre.next = pre.next.next
-        return dummy.next
+        # pre = dummy
+        # for i in range(count-n):
+        #     pre = pre.next
+        # pre.next = pre.next.next
+        # return dummy.next
+
+        slow = head
+        fast = head
+        i=1
+        for i in range(n):
+            if fast is None:
+                return head
+            fast = fast.next
+            
+        if fast == None:
+            head = head.next
+            return head
+            
+        while fast.next:
+            slow =slow.next
+            fast = fast.next
+        slow.next = slow.next.next
+        return head 
