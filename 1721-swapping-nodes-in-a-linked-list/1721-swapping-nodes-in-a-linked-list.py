6c689255c631a1a33577003ab969fd258fc5c4f6
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2025-08-16 21:09:53 +0100

    Time: 38 ms (67.89%), Space: 40.1 MB (72.21%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.py
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.py
@@ -23,24 +23,17 @@ class Solution:
         if not head:
             return head
 
-        # 1) Find k-th from start
         first = head
-        for _ in range(k - 1):
+        for i in range(k-1):
             first = first.next
-
-        # 2) Find k-th from end using two-pointer technique
-        fast = head
-        for _ in range(k):
-            fast = fast.next  # move k steps ahead
-
-        second = head
-        while fast:
-            fast = fast.next
+        second = first
+        temp = head
+        while second.next:
+            temp = temp.next
             second = second.next
-
-        # 3) Swap values
-        first.val, second.val = second.val, first.val
+        second = temp
+        first.val, second.val = second.val , first.val
         return head
-        
+
 
             
