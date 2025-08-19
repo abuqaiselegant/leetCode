ec98b129de93d118aed27d243d4f4d4c9a62eb84
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2025-08-19 03:09:38 +0100

    Time: 81 ms (10.89%), Space: 18.7 MB (45.66%) - LeetHub

---
+++ 0707-design-linked-list/0707-design-linked-list.py
@@ -0,0 +1,84 @@
+class Node:
+    def __init__(self,val):
+        self.val = val
+        self.next = None
+
+
+class MyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def get(self, index: int) -> int:
+        count = 0
+        temp = self.head
+        while temp:
+            if count == index:
+                return temp.val
+            temp = temp.next
+            count += 1
+        return -1
+
+    def addAtHead(self, val: int) -> None:
+        temp = Node(val)
+        temp.next = self.head
+        self.head = temp
+
+    def addAtTail(self, val: int) -> None:
+        temp = self.head
+        tail = Node(val)
+        if temp is None:
+            self.head = tail
+            return
+        while temp.next:
+            temp = temp.next
+        temp.next = tail
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        newNode = Node(val)
+        if index == 0:
+            newNode.next = self.head
+            self.head = newNode
+            return 
+        
+        # traverse to index-1
+        temp = self.head
+        count = 0
+        while temp and count < index-1:
+            temp = temp.next
+            count += 1
+
+        if temp is None:   # index > length
+            return
+
+        newNode.next = temp.next
+        temp.next = newNode
+
+    def deleteAtIndex(self, index: int) -> None:
+        if self.head is None:
+            return
+        if index == 0:
+            self.head = self.head.next
+            return
+        
+        # traverse to index-1
+        temp = self.head
+        count = 0
+        while temp and count < index-1:
+            temp = temp.next
+            count += 1
+
+        if temp is None or temp.next is None:  # index out of range
+            return
+
+        temp.next = temp.next.next
+
+        
+
+
+# Your MyLinkedList object will be instantiated and called as such:
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
