40f6d4a9c22289df994a211cf3df67ac57004e35
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2025-08-20 23:46:17 +0100

    Time: 4 ms (80.39%), Space: 22.5 MB (5.62%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,32 +1,39 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        if len(nums)==1:
-            return [nums[0]]
-        x, y , count1, count2 = None, None, 0,0
-        for num in nums:
-            if x==num:
-                count1+=1
-            elif y ==num:
-                count2+=1
-            elif count1 == 0:
-                x = num
-                count1 =1
-            elif count2 ==0:
-                y = num
-                count2 =1
-            else:
-                count1-=1
-                count2-=1
-        a = []
-        count1=0
-        count2=0
-        for num in nums:
-            if num ==x:
-                count1+=1
-            elif num ==y:
-                count2+=1
-        if count1 >len(nums)/3:
-            a.append(x)
-        if count2 >len(nums)/3:
-            a.append(y)
-        return a
+        # if len(nums)==1:
+        #     return [nums[0]]
+        # x, y , count1, count2 = None, None, 0,0
+        # for num in nums:
+        #     if x==num:
+        #         count1+=1
+        #     elif y ==num:
+        #         count2+=1
+        #     elif count1 == 0:
+        #         x = num
+        #         count1 =1
+        #     elif count2 ==0:
+        #         y = num
+        #         count2 =1
+        #     else:
+        #         count1-=1
+        #         count2-=1
+        # a = []
+        # count1=0
+        # count2=0
+        # for num in nums:
+        #     if num ==x:
+        #         count1+=1
+        #     elif num ==y:
+        #         count2+=1
+        # if count1 >len(nums)/3:
+        #     a.append(x)
+        # if count2 >len(nums)/3:
+        #     a.append(y)
+        # return a
+        n = len(nums)
+        freq = Counter(nums)
+        result = []
+        for i in freq:
+            if freq[i]>n//3:
+                result.append(i)
+        return result
