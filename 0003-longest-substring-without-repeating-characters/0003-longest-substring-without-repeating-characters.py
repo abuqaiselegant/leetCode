36d503f75e2de184b2d26388e1dfc3bd6b3e5a2e
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2025-08-29 02:44:40 +0100

    Time: 13 ms (86.25%), Space: 17.6 MB (96.73%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,15 +1,61 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        a = set()
+        l =0
+        r=0
         maxLen = 0
-        for i in range(len(s)):
-            a =set()
-            for j in range(i,len(s)):
-                if s[j] in a:
-                    i+=1
-                    break
-                maxLen = max(maxLen, j-i+1)
-                a.add(s[j])
+        while r<len(s):
+            while s[r] in a:
+                a.remove(s[l])
+                l+=1
+            a.add(s[r])
+
+            maxLen=max(maxLen, r-l+1)
+            r+=1
         return maxLen
+
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+        # l= 0 
+        # a=set()
+        # maxLen=0
+        # for r in range(len(s)):
+        #     while s[r] in a:
+        #         a.remove(s[l])
+        #         l+=1
+        #     a.add(s[r])
+        #     maxLen = max(maxLen,r-l+1)
+        # return maxLen
+
+        
+        
+        
+        
+        
+        
+        # maxLen = 0
+        # for i in range(len(s)):
+        #     a =set()
+        #     for j in range(i,len(s)):
+        #         if s[j] in a:
+        #             i+=1
+        #             break
+        #         maxLen = max(maxLen, j-i+1)
+        #         a.add(s[j])
+        # return maxLen
                 
         
         
