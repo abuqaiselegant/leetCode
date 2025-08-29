bd141fe611d08def141ce666b3ee3b03b9b24988
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2025-08-29 02:06:24 +0100

    Time: 107 ms (89.26%), Space: 18.1 MB (49.3%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,17 +1,40 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        count={}
-        l=0
-        res=0
-        maxf=0
-
+        freq = {}
+        l = 0
+        maxLen = 0
+        maxCount = 0
         for r in range(len(s)):
-            count[s[r]]=count.get(s[r],0)+1
-            maxf=max(maxf,count[s[r]])
-
-            while (r-l+1)-maxf >k:
-                count[s[l]]-=1
+            freq[s[r]]=freq.setdefault(s[r],0)+1
+            maxCount = max(freq[s[r]],maxCount)
+            while r-l+1-maxCount>k:
+                freq[s[l]]-=1
                 l+=1
-            res = max(maxf,r-l+1)
-        return res
+            maxLen =max(maxLen,r-l+1)
+        return maxLen
+
+
+
+
+
+
+
+
+
+
+        
+        # count={}
+        # l=0
+        # res=0
+        # maxf=0
+
+        # for r in range(len(s)):
+        #     count[s[r]]=count.get(s[r],0)+1
+        #     maxf=max(maxf,count[s[r]])
+
+        #     while (r-l+1)-maxf >k:
+        #         count[s[l]]-=1
+        #         l+=1
+        #     res = max(maxf,r-l+1)
+        # return res
         
