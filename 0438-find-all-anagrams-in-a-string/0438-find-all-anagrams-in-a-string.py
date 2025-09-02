76a85224eb125db9736231061b7272c4ec875335
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2025-09-02 20:31:46 +0100

    Time: 227 ms (31.7%), Space: 18.2 MB (79.16%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -2,31 +2,26 @@ from collections import Counter
 
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
+
         # answer = []
         # for r in range(len(s)-len(p)+1):
         #     a = s[r:r+len(p)]
         #     if sorted(a)==sorted(p):
         #         answer.append(r)
         # return answer
+
         k = len(p)
         n = len(s)
-        if n<k:
-            return []
-        
-        p_count = Counter(p)
-        window_count = Counter(s[:k-1])
         answer = []
-
+        window = Counter(s[:k-1])
+        p_count = Counter(p)
+        
         for i in range(k-1, n):
-            window_count[s[i]] += 1
-
-            if window_count == p_count:
-                answer.append(i-k+1)
-            
-            left_char = s[i-k+1]
-            window_count[left_char] -= 1
-            if window_count[left_char] == 0:
-                del window_count[left_char]
-        
+            window[s[i]] += 1
+            if window == p_count:
+                answer.append(i+1-k)
+            leftChar = i+1-k
+            window[s[leftChar]]-=1
+            if window[s[leftChar]]==0:
+                del window[s[leftChar]]
         return answer
-        
