7df0697117d1025359a1455bbeadbc2ae6b75e98
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2025-09-02 20:45:02 +0100

    Time: 189 ms (41.59%), Space: 18.3 MB (57.2%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -10,18 +10,36 @@ class Solution:
         #         answer.append(r)
         # return answer
 
-        k = len(p)
-        n = len(s)
-        answer = []
-        window = Counter(s[:k-1])
-        p_count = Counter(p)
+        # k = len(p)
+        # n = len(s)
+        # answer = []
+        # window = Counter(s[:k-1])
+        # p_count = Counter(p)
         
-        for i in range(k-1, n):
-            window[s[i]] += 1
-            if window == p_count:
-                answer.append(i+1-k)
-            leftChar = i+1-k
-            window[s[leftChar]]-=1
-            if window[s[leftChar]]==0:
-                del window[s[leftChar]]
-        return answer
+        # for i in range(k-1, n):
+        #     window[s[i]] += 1
+        #     if window == p_count:
+        #         answer.append(i+1-k)
+        #     leftChar = i+1-k
+        #     window[s[leftChar]]-=1
+        #     if window[s[leftChar]]==0:
+        #         del window[s[leftChar]]
+        # return answer
+
+        window_len = len(p)
+        if window_len > len(s):
+            return []
+        anagram = Counter(p)
+        window = Counter(s[0:window_len])
+        anagrams = []
+        if window == anagram:
+            anagrams.append(0)
+        
+        for r in range(window_len, len(s)):
+            l = r - window_len
+            window[s[r]] += 1
+            window[s[l]] -= 1
+            l += 1
+            if window == anagram:
+                anagrams.append(l)
+        return anagrams
