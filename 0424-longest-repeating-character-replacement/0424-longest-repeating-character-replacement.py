763f0bf3ad01e4b367082bbc92d54fecb6793fc2
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2025-09-23 14:03:01 +0100

    Time: 130 ms (47.88%), Space: 18.2 MB (23.8%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -2,39 +2,30 @@ class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         freq = {}
         l = 0
-        maxLen = 0
+        r = 0
         maxCount = 0
-        for r in range(len(s)):
+        maxLen =0
+        while r <len(s):
             freq[s[r]]=freq.setdefault(s[r],0)+1
-            maxCount = max(freq[s[r]],maxCount)
-            while r-l+1-maxCount>k:
+            maxCount = max(maxCount, freq[s[r]])
+            while (r-l+1 - maxCount) > k:
                 freq[s[l]]-=1
                 l+=1
-            maxLen =max(maxLen,r-l+1)
+            
+            maxLen = max(maxLen, r-l+1)
+            r+=1
         return maxLen
 
-
-
-
-
-
-
-
-
-
         
-        # count={}
-        # l=0
-        # res=0
-        # maxf=0
-
+        # freq = {}
+        # l = 0
+        # maxLen = 0
+        # maxCount = 0
         # for r in range(len(s)):
-        #     count[s[r]]=count.get(s[r],0)+1
-        #     maxf=max(maxf,count[s[r]])
-
-        #     while (r-l+1)-maxf >k:
-        #         count[s[l]]-=1
+        #     freq[s[r]]=freq.setdefault(s[r],0)+1
+        #     maxCount = max(freq[s[r]],maxCount)
+        #     while r-l+1-maxCount>k:
+        #         freq[s[l]]-=1
         #         l+=1
-        #     res = max(maxf,r-l+1)
-        # return res
-        
+        #     maxLen =max(maxLen,r-l+1)
+        # return maxLen
