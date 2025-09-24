427b4c7ccc4329497f2bf2f5e71db3bbdfac2e43
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2025-09-24 02:56:13 +0100

    Time: 23 ms (8.86%), Space: 28.3 MB (36.78%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -1,5 +1,22 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        l = 0
+        r = 0
+        min_len = float('inf')
+        total = 0
+        while r < len(nums):
+            total += nums[r]
+            while total >= target:
+                min_len = min(min_len, r-l+1)
+                print(min_len)
+                total -= nums[l]
+                print(total)
+                l+=1
+            r+=1
+        return 0 if min_len == float('inf') else min_len
+            
+        
+        
         # l=0
         # min_len = float('inf')
         # total = 0
@@ -11,15 +28,15 @@ class Solution:
         #         l+=1
         # return 0 if min_len ==float('inf') else min_len
 
-        l = 0
-        r = 0
-        minlen = float('inf')
-        currSum = 0
-        while r <len(nums):
-            currSum += nums[r]
-            while currSum>=target:
-                minlen =min(minlen,r-l+1)
-                currSum-=nums[l]
-                l+=1
-            r+=1
-        return 0 if minlen ==float('inf') else minlen
+        # l = 0
+        # r = 0
+        # minlen = float('inf')
+        # currSum = 0
+        # while r <len(nums):
+        #     currSum += nums[r]
+        #     while currSum>=target:
+        #         minlen =min(minlen,r-l+1)
+        #         currSum-=nums[l]
+        #         l+=1
+        #     r+=1
+        # return 0 if minlen ==float('inf') else minlen
