d6ecc5257374b292713012178d3c1f8b74996aec
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2025-09-24 20:49:27 +0100

    Time: 0 ms (100%), Space: 19.2 MB (32.41%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -1,37 +1,11 @@
 class Solution:
-    def binary_search_left(self, arr, x):
-        left, right = 0, len(arr)   # search range [0, n)
-        while left < right:
-            mid = (left + right) // 2
-            if arr[mid] < x:
-                left = mid + 1
-            else:
-                right = mid
-        return left
-
-    def findClosestElements(self, arr, k, x):
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
         n = len(arr)
-        
-        # 1. Find insertion point
-        pos = self.binary_search_left(arr, x)
-        
-        # 2. Initialize pointers
-        if pos < n and arr[pos] == x:
-            left = right = pos
-        else:
-            left, right = pos - 1, pos
-        
-        # 3. Expand outward until we have k elements
-        while (right - left - 1) < k:
-            if left < 0:
-                right += 1
-            elif right >= n:
-                left -= 1
+        l , r = 0,n-k
+        while l<r:
+            mid = (l+r)//2
+            if x-arr[mid] > arr[mid+k]-x:
+                l = mid +1
             else:
-                if abs(arr[left] - x) <= abs(arr[right] - x):
-                    left -= 1
-                else:
-                    right += 1
-        
-        # 4. Return the window
-        return arr[left+1:right]
+                r = mid
+        return arr[l:l+k]
