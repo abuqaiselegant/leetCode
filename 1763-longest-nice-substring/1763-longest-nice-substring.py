9cca00628586ec18ab15c1ec7533edb8d41ecf94
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2025-10-07 19:48:18 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1763-longest-nice-substring/1763-longest-nice-substring.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestNiceSubstring(self, s: str) -> str:
+        c = set(s)
+
+        for i, ch in enumerate(s):
+            if ch.islower():
+                opposite = chr(ord(ch)-32)
+            else:
+                opposite = chr(ord(ch)+32)
+            
+            if opposite not in c:
+                leftPart = self.longestNiceSubstring(s[:i])
+                rightPart = self.longestNiceSubstring(s[i+1:])
+                return leftPart if len(leftPart)>len(rightPart) else rightPart
+        return s
+        
