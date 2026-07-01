8597b33e15828a2b9aaa70242584eb3eaff54cc2
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2026-07-01 17:50:27 +0530

    Time: 19 ms (8.53%), Space: 19.3 MB (17.89%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,43 +1,13 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
-        if x<0 or (x!=0 and x%10==0):
-            return False
-        revHalf =0
-        while x>revHalf:
-            revHalf = revHalf*10+x%10
-            x//=10
-        if x==revHalf or x ==revHalf//10:
-            return True
-        else :
-            return False
-        """
-        __________
-
-        if x<0:
-            return False
-        if str(x)==str(x)[::-1] :
-            return True
-        else :
-            return False
-        ---------- 
-        if x<0:
-            return False
-
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
         rev = 0
         temp = x
         while temp>0:
             rem = temp%10
             rev = rev*10+rem
             temp=temp//10
+        
         if rev == x:
             return True
-        else :
+        else:
             return False
-        """
-
-        
-        
