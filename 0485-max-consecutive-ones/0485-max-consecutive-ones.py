e88b4998f974f1695975ae6d08b37f364d85d336
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2026-07-01 18:17:56 +0530

    Time: 15 ms (65.87%), Space: 21.7 MB (79.53%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.py
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.py
@@ -3,27 +3,9 @@ class Solution:
         maxCount = 0
         count = 0
         for x in nums:
-            if x ==1:
-                count+=1
+            if x == 1:
+                count +=1
                 maxCount = max(count, maxCount)
             else:
                 count = 0
-
         return maxCount
-        
-        
-        
-        
-        
-        
-        
-        
-        # maxCount =0
-        # count =0
-        # for i in nums:
-        #     if i ==1:
-        #         count+=1
-        #         maxCount = max(maxCount,count)
-        #     else: count =0
-
-        # return maxCount
