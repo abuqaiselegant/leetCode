e6c23b2ffe6d26f54469a4894317233109c871ec
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2026-07-06 19:07:17 +0530

    Time: 0 ms (100%), Space: 19.2 MB (60.3%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,53 +3,18 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        """count0 =0
-        count1 =0
-        count2 =0
-        for i in range(len(nums)):
-            if nums[i]==0:
-                count0+=1
-            elif nums[i]==1:
-                count1+=1
-            else:
-                count2+=1
-        for i in range(count0):
-            nums[i]=0
-        for i in range(count0,count1+count0):
-            nums[i]=1
-        for i in range(count0+count1,count0+count1+count2):
-            nums[i]=2"""
-        # #this is dutch national flag algorithm
-        # low = 0
-        # mid = 0
-        # high = len(nums)-1
-        # while mid<=high:
-        #     if nums[mid]==0:
-                
-        #         temp = nums[mid]
-        #         nums[mid] = nums[low]
-        #         nums[low] = temp
-        #         low+=1
-        #         mid+=1
-        #     elif nums[mid]==1:
-        #         mid+=1
-        #     else:
-        #         temp = nums[high]
-        #         nums[high]= nums[mid]
-        #         nums[mid]=temp
-        #         high-=1
         left = 0
+        mid = 0
         right = len(nums)-1
-        i = 0
-        while i <= right:
-            if nums[i]==0:
-                nums[left], nums[i]=nums[i],nums[left]
+        while mid<=right:
+            if nums[mid]==0:
+                nums[left], nums[mid] = nums[mid], nums[left]
                 left+=1
-                i+=1
-            elif nums[i]==2:
-                nums[i],nums[right]=nums[right],nums[i]
-                right-=1
+                mid+=1
+            elif nums[mid]==1:
+                mid+=1
             else:
-                i+=1 
-    
+                nums[right],nums[mid] = nums[mid], nums[right]
+                right-=1
+        
         
