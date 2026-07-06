7d87743b64a6e8bae1056839e074fb5f74240cf1
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2026-07-06 19:28:16 +0530

    Time: 1399 ms (8.34%), Space: 20.3 MB (95.04%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,23 +1,6 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        n = len(nums)
-        """for i in range(n+1):
-            flag =0
-            for j in range(n):
-                if nums[j]==i:
-                    flag =1
-                    break
-            if flag==0:
+        for i in range(1,len(nums)+1):
+            if i not in nums:
                 return i
-
-        hash = [0]*(n+1)
-        for i in range(n):
-            hash[nums[i]]+=1
-        for i in range(n+1):
-            if hash[i]==0:
-                return i"""
-                
-        summation = (n * (n + 1)) // 2
-        s2 = sum(nums)
-        return summation-s2
-
+        return 0
