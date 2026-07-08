c1e05432e8790c4ce0d6b58cdc71d17c47c426d6
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2026-07-09 01:26:29 +0530

    Time: 19 ms (38.09%), Space: 20.6 MB (66.84%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -13,12 +13,26 @@ class Solution:
         #         ans.append(i)
                 
         # return ans
-        seen = set()
-        duplicate = 0
-        for num in nums:
-            if num in seen:
-                duplicate = num
-            seen.add(num)
-        for i in range(1, len(nums) + 1):
-            if i not in seen:
-                return [duplicate, i]
+
+        # seen = set()
+        # duplicate = 0
+        # for num in nums:
+        #     if num in seen:
+        #         duplicate = num
+        #     seen.add(num)
+        # for i in range(1, len(nums) + 1):
+        #     if i not in seen:
+        #         return [duplicate, i]
+
+        n = len(nums)
+        i = 0
+        while i <n:
+            index = nums[i]-1
+            if 1<=nums[i]<=n and nums[index] != nums[i]:
+                nums[index], nums[i] = nums[i], nums[index]
+            else: 
+                i+=1
+        for i in range(n):
+            if nums[i] != i+1:
+                return [nums[i], i+1]
+        return [nums[n],n+1]
