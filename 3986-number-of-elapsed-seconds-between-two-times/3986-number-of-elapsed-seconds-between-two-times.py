ab84a9bc11fd6e36d77f005d862bab1090c09b61
Abu Qais <66121156+abuqaiselegant@users.noreply.github.com>
2026-07-14 11:05:01 +0530

    Time: 3 ms (25.9%), Space: 19.2 MB (65.18%) - LeetHub

--- 3986-number-of-elapsed-seconds-between-two-times/3986-number-of-elapsed-seconds-between-two-times.py
+++ 3986-number-of-elapsed-seconds-between-two-times/3986-number-of-elapsed-seconds-between-two-times.py
@@ -1,9 +1,10 @@
 class Solution:
     def secondsBetweenTimes(self, startTime: str, endTime: str) -> int:
        
-        
-        def fetchSecond(x):
-            h,m,s = map(lambda x:int(x), x.split(":"))
-            return h*60*60 + m*60 +s
-        diff = fetchSecond(endTime) - fetchSecond(startTime) 
+        diff = self.fetchSecond(endTime) - self.fetchSecond(startTime) 
         return diff
+
+    def fetchSecond(self,x):
+        h,m,s = map(lambda x:int(x), x.split(":"))
+        return h*60*60 + m*60 +s
+    
